f1dc115f45526a1a7e9e620102b051a44a18e149
Irina Teremenko <irina.teremenko@gmail.com>
2023-03-30 18:34:33 +0300

    Created two classes with data formatted output

---
+++ src/IntObject.java
@@ -0,0 +1,9 @@
+public class IntObject {
+
+    private int value = 777;
+
+    public  int GetValue() {
+        System.out.println(String.format("---%d---", value));
+        return value;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        IntObject obj = new IntObject();
+        System.out.println(String.format("~~~%d~~~", obj.GetValue()));
+    }
+
+}
